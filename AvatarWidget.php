af1bc4d9c9fb26a4625695db67ed0229b706cd31
huanguang <923414405@qq.com>
2017-04-11 17:09:21 +0800

    修改次数

---
+++ AvatarWidget.php
@@ -0,0 +1,28 @@
+<?php
+namespace huanguang\avatar;
+/**
+ * 设置挂件
+ * @author 上班偷偷打酱油 （xianan_huang@163.com）
+ * @copyright Yii中文网 （www.yii-china.com）
+ */
+use Yii;
+use yii\bootstrap\Widget;
+use hyii2\avatar\assets\AvatarAsset;
+use yii\base\Object;
+
+class AvatarWidget extends Widget
+{    
+    public $imageUrl = '';
+    
+    public function run()
+    {
+        $this->registerClientScript();
+        $model = new UploadForm();        
+        return $this->render('index',['model'=>$model]);
+    }
+    
+    public function registerClientScript()
+    {
+        AvatarAsset::register($this->view);
+    }
+}
